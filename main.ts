afb421b21b368d8c34863adee078660f2ad1513b
quocbinhdlu <72564711+quocbinhdlu@users.noreply.github.com>
2020-10-08 21:53:07 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+// 1 lần vào lúc bắt đầu
+basic.showIcon(IconNames.Heart)
+basic.pause(2000)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
